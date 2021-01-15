ec5f852184964fbba2efc331bc2b494e63a11827
g00u00 <g00u00@outlook.com>
2021-01-15 09:29:20 +0300

    Create cgi_test.sh

---
+++ cgi-bin/cgi_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#https://www.tldp.org/LDP/abs/html/index.html
+#https://ss64.com/bash/
+echo "Content-type: text/html"
+echo ""
+echo '<!DOCTYPE html>'
+echo '<html>'
+echo '<head>'
+echo '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">'
+echo '<title>cgi_test</title>'
+echo '</head>'
+echo '<body>'
+echo '<pre>'
+echo "Работает"
+echo '</pre>'
+echo '</html>'
