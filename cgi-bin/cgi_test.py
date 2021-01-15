19e06c3e0a609a44e9a8b7f405b0255ead1d2004
g00u00 <g00u00@outlook.com>
2021-01-15 09:30:21 +0300

    Create cgi_test.py

---
+++ cgi-bin/cgi_test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3.8
+print("Content-type:text/html\r\n")
+print('''\
+<!DOCTYPE html>\n<html>\n<head>\n<title>g06u33_CGI_тестируем</title>\n</head>
+<body>\n<pre>
+Html-страница сгенерирована
+<pre>\n<body>\n<html>
+''')
